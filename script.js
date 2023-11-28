af59bf242ea640f267ce6bc487ab2798613d5e58
namanshukla8707 <113860823+namanshukla8707@users.noreply.github.com>
2023-11-28 19:57:56 +0530

    Added Resume and Reset Button

--- script.js
+++ script.js
@@ -2,21 +2,24 @@
 const minutesInput = document.getElementById("minutes");
 const secondsInput = document.getElementById("seconds");
 const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resumeBtn = document.getElementById("resumeBtn"); // New resume button
 const resetBtn = document.getElementById("resetBtn");
 const timerDisplay = document.getElementById("timer");
 
 let timerInterval;
 let totalTime = 0;
-
+let isActive = false;
 
 startBtn.addEventListener("click", startTimer);
-
+stopBtn.addEventListener("click", stopTimer);
+resumeBtn.addEventListener("click", resumeTimer); // Event listener for resume button
 
 function startTimer() {
   const minutes = parseInt(minutesInput.value);
   const seconds = parseInt(secondsInput.value);
-  
-  if ((!minutes&&minutes!==0) || (!seconds&&seconds!==0)) {
+
+  if ((!minutes && minutes !== 0) || (!seconds && seconds !== 0)) {
     alert("Please enter a valid time");
     return;
   }
@@ -33,8 +36,20 @@ function startTimer() {
   totalTime = minutes * 60 + seconds;
   clearInterval(timerInterval);
   timerInterval = setInterval(updateTimer, 1000);
+  isActive = true;
 }
 
+function stopTimer() {
+  clearInterval(timerInterval);
+  isActive = false;
+}
+
+function resumeTimer() {
+  if (!isActive) {
+    timerInterval = setInterval(updateTimer, 1000);
+    isActive = true;
+  }
+}
 
 function updateTimer() {
   let minutes = Math.floor(totalTime / 60);
@@ -52,11 +67,11 @@ function updateTimer() {
   }
 }
 
-
 resetBtn.addEventListener("click", function () {
   clearInterval(timerInterval);
   minutesInput.value = "";
   secondsInput.value = "";
   timerDisplay.textContent = "00:00";
   totalTime = 0;
+  isActive = false;
 });
